7f3d92d4eeea5b745a9c16f5b7ec14e4944f01a6
GRenoldi <guilherme.renoldi@gmail.com>
2021-01-20 16:01:06 -0300

    Creation of the first html templates for the agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from django.views.generic import RedirectView
 
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventos/<titulo_evento>', views.getLocal),
+    path('eventos/<titulo_evento>', views.get_local),
+    path('agenda/', views.lista_eventos),
+    path('', views.index),
+    # path('', RedirectView.as_view(url='/agenda/'))
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 from core.models import Evento
 
 
-def getLocal(request, titulo_evento):
+def get_local(request, titulo_evento):
     evento = Evento.objects.get(titulo=titulo_evento)
     return HttpResponse(evento.local)
+
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
+
+
+def index(request):
+    return redirect('/agenda/')
